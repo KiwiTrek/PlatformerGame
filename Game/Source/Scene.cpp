8cb29e13bab24431a1719bc3b96d58df8bbfc9a8
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2020-10-23 09:33:50 +0200

    coding style thingy

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -53,25 +53,33 @@ bool Scene::Update(float dt)
 {
 
 	if (app->input->GetKey(SDL_SCANCODE_F6) == KEY_DOWN)
+	{
 		app->LoadRequest();
-
+	}
 	if (app->input->GetKey(SDL_SCANCODE_F5) == KEY_DOWN)
+	{
 		app->SaveRequest();
-
+	}
 	if (app->input->GetKey(SDL_SCANCODE_M) == KEY_DOWN)
+	{
 		app->audio->MuteVolume();
-
-	if(app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	}
+	if (app->input->GetKey(SDL_SCANCODE_UP) == KEY_REPEAT)
+	{
 		app->render->camera.y += 1;
-
-	if(app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	}
+	if (app->input->GetKey(SDL_SCANCODE_DOWN) == KEY_REPEAT)
+	{
 		app->render->camera.y -= 1;
-
-	if(app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	}
+	if (app->input->GetKey(SDL_SCANCODE_LEFT) == KEY_REPEAT)
+	{
 		app->render->camera.x += 1;
-
-	if(app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	}
+	if (app->input->GetKey(SDL_SCANCODE_RIGHT) == KEY_REPEAT)
+	{
 		app->render->camera.x -= 1;
+	}
 
 	app->map->Draw();
 
